816f6ac2883aca50db096e77a11fd6d4c899c0b0
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-23 16:14:50 -0400

    Update counter.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -12,14 +12,21 @@ class Counter extends Component {
 
 
 
-  render() {
+    render() {
+        
     return (
       <React.Fragment>
-            <span style={this.styles } className = "badge badge-primary m-2">{this.formatCount()}</span>
-        <button className = "btn btn-secondary  btn-sm">Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary  btn-sm">Increment</button>
       </React.Fragment>
     );
   }
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
