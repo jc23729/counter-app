a1ef243600c1522fa42a552fc79ad5b60519509f
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-23 15:04:16 -0400

    Update counter.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,15 +2,13 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-      count: 0,
-      imageUrl: "https://picsum.photos/200"
+    count: 0,
   };
   render() {
     return (
-        <React.Fragment>
-            <img src="{this.state.imageUrl}" alt=""/>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+      <React.Fragment>
+        <span className = "badge badge-primary m-2">{this.formatCount()}</span>
+        <button className = "btn btn-secondary  btn-sm">Increment</button>
       </React.Fragment>
     );
   }
@@ -21,6 +19,5 @@ class Counter extends Component {
 }
 
 export default Counter;
-
 
 //so for this.state.count your refrencing the current object on the class Counter extends
