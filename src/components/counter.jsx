1802e06f96c929d23b27ecc21eaa2361f22cd81a
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-24 12:06:42 -0400

    Update counter.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -14,28 +14,28 @@ class Counter extends Component {
   render() {
     return (
       <React.Fragment>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary  btn-sm">Increment</button>
         <ul>
           {this.state.tags.map((tag) => (
-              <li>{tag}</li>
+            <li key={tag}>{tag}</li>
           ))}
         </ul>
       </React.Fragment>
     );
   }
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
+//   getBadgeClasses() {
+//     let classes = "badge m-2 badge-";
+//     classes += this.state.count === 0 ? "warning" : "primary";
+//     return classes;
+//   }
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
-  }
+//   formatCount() {
+//     const { count } = this.state;
+//     return count === 0 ? "Zero" : count;
+//   }
 }
 
 export default Counter;
 
 //so for this.state.count your refrencing the current object on the class Counter extends
+//    <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+//         <button className="btn btn-secondary  btn-sm">Increment</button>
